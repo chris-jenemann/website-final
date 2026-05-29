b1713dbcc9b6d7938b3b0afb8a98248fd2a4974b
Christopher Jenemann <chrisjenemann@gmail.com>
2026-05-29 15:52:32 -0600

    some optimizations

--- website/script.js
+++ website/script.js
@@ -7,7 +7,7 @@ function increaseBlur()
   const scrollProgress = window.scrollY / (window.innerHeight * 2);
   const maxBlur = window.innerHeight * 0.1; // max blur at bottom of page
   const blurValue = scrollProgress * maxBlur;
-  const brightnessValue= Math.max(Math.abs(1-scrollProgress),.5);
+  const brightnessValue=Math.max(1 - scrollProgress, 0.5);
   blur.setAttribute("style", `backdrop-filter: blur(${blurValue}px)`);
   backdrop.style.filter = `brightness(${brightnessValue})`;
   console.log( brightnessValue);
@@ -23,6 +23,15 @@ aboutSection.scrollIntoView({behavior: "smooth"})
 
 
 
+let ticking = false;
+window.addEventListener('scroll', () => {
+  if (!ticking) {
+    requestAnimationFrame(() => {
+      increaseBlur();
+      ticking = false;
+    });
+    ticking = true;
+  }
+});
 
-window.addEventListener('scroll',increaseBlur)
 button.addEventListener('click', scrollDown)
